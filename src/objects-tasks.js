c9d5f2dab29e3d6c77df72ab43dd5acbef906372
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-03-17 22:23:29 +0300

    feat: implement 9 and 10 subtasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -208,8 +208,14 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => {
+    return this.width * this.height;
+  };
+
+  return this;
 }
 
 /**
@@ -222,8 +228,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
