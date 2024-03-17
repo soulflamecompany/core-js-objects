f4796cdf22b53a34d7d54451ef8fd03b39914052
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-03-18 00:26:13 +0300

    feat: implement 11 subtask

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -328,8 +328,18 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((map, item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(value);
+
+    return map;
+  }, new Map());
 }
 
 /**
